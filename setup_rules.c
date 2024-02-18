877bed8f2f4e71168676a96cb173680fe18584cd
Oualid Hassani <ohassani@c4r4p10.1337.ma>
2024-02-18 11:20:59 +0100

    add some rules

--- setup_rules.c
+++ setup_rules.c
@@ -2,11 +2,35 @@
 
 void sa(node_t **top)
 {
+    int tmp;
+
     if((*top) == NULL || (*top)->next == NULL)
         return ;
-    int tmp;
 
     tmp = (*top)->val;
     (*top)->val = (*top)->next->val;
     (*top)->next->val = tmp;
+}
+
+void sb(node_t **topb)
+{
+    if((*topb) == NULL || (*topb) ->next == NULL)
+        return ;
+    
+    int tmpb;
+
+    tmpb = (*topb)->val;
+    (*topb)->val == (*topb)->next->val;
+    (*topb)->next->val = tmpb;
+}
+
+void ss(node_t **top, node_t **topb)
+{
+    sa(top);
+    sb(topb);
+}
+
+void pa(node_t **top, node_t **topb)
+{
+    
 }
